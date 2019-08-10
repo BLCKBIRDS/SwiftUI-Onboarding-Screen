f5d86c956b378bf1112108db3f76e55769c1211a
BLCKBIRDS <andreasschultz@Andreass-MBP.fritz.box>
2019-08-11 00:34:21 +0200

    Init
    Init

---
+++ Part1/App Onboarding/App Onboarding/OnboardingView.swift
@@ -0,0 +1,31 @@
+//
+//  OnboardingView.swift
+//  App Onboarding
+//
+//  Created by Andreas Schultz on 10.08.19.
+//  Copyright © 2019 Andreas Schultz. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    var subviews = [
+        UIHostingController(rootView: Subview(imageString: "meditating")),
+        UIHostingController(rootView: Subview(imageString: "skydiving")),
+        UIHostingController(rootView: Subview(imageString: "sitting"))
+    ]
+    
+    var body: some View {
+        PageViewController(viewControllers: subviews)
+            .frame(height: 600)
+    }
+}
+
+#if DEBUG
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
+#endif
---
+++ Part1/App Onboarding/App Onboarding/Subview.swift
@@ -0,0 +1,29 @@
+//
+//  Subview.swift
+//  App Onboarding
+//
+//  Created by Andreas Schultz on 10.08.19.
+//  Copyright © 2019 Andreas Schultz. All rights reserved.
+//
+
+import SwiftUI
+
+struct Subview: View {
+    
+    var imageString: String
+    
+    var body: some View {
+        Image(imageString)
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .clipped()
+    }
+}
+
+#if DEBUG
+struct Subview_Previews: PreviewProvider {
+    static var previews: some View {
+        Subview(imageString: "meditating")
+    }
+}
+#endif
